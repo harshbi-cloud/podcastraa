e1de921d8c4fc8e3a5d20ad1f29bcaa110e85cd9
Harshvardhan Dwivedi <144583555+harshbi-cloud@users.noreply.github.com>
2024-12-03 16:24:28 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div classNamme="mt-9 flex flex-col gap-9">
+    
+    <section> className='flex flex-col gap-9' 
+   <h1 className="text-20 font-bold text-white-1"> Trending Podcast</h1>
+   <Button variant="outline">Button</Button>
+Copy
+Link
+   </section>
+    </div>
+  )
+}
+
+export default home
